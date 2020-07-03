736a088365479806a6d3be1961215d8a6168a295
gabrieldpg <gabrielpcpg@gmail.com>
2020-07-04 11:10:57 +1200

    added constants for document options available for now

---
+++ sending-options/src/constants/documentOptions.js
@@ -0,0 +1,13 @@
+export const ACCOUNT_CLOSURE = 'Account Closures';
+export const AUTHORISED_PERSON_FORM = 'Authorised Person Form';
+export const AUTOMATIC_PAYMENT_FORM = 'Automatic Payment Form';
+export const BANK_ACCOUNT_DETAILS = 'Bank Account Details';
+export const CHANGE_OF_NAME = 'Change of Name';
+export const CREDIT_LIMIT_CONFIRMATION = 'Credit Limit Confirmation';
+export const DEALER_CANCELLATION_FORM = 'Dealer Cancellation Form';
+export const DIRECT_DEBIT_FORM = 'Direct Debit Form';
+export const DISPUTED_TRANSACTION_FORM = 'Disputed Transaction Form';
+export const FINAL_SETTLEMENT_FIGURE = 'Final Settlement Figure';
+export const PAYMENT_CONFIRMATION = 'Payment Confirmation';
+export const PURCHASE_CONFIRMATION = 'Purchase Confirmation';
+export const WSS_REGISTRATION = 'WSS Registration';
